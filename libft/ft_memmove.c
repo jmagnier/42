7574427f7925219ddd9407bb0ce1ba3560cc743a
Jeremy MAGNIER <jmagnier@e3r3p11.42.fr>
2016-01-10 17:31:23 +0100

    10janvier2016,17h30

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -3,10 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmagnier <jmagnier@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jmagnier <jmagnier@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/12/22 19:03:13 by jmagnier          #+#    #+#             */
-/*   Updated: 2015/12/22 19:03:50 by jmagnier         ###   ########.fr       */
+/*   Created: 2015/12/21 17:43:59 by jmagnier          #+#    #+#             */
+/*   Updated: 2016/01/10 17:27:27 by jmagnier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string.h>
+
+void    *memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char *dsttemp;
+	unsigned char *srctemp;
+
+	dsttemp = (unsigned char *)dst;
+	srctemp = (unsigned char *)src;
+
+	if (srctemp <= desttemp)
+	{
+		desttemp += n - 1;
+		srctemp += n - 1;
+
+		while (n--)
+			*desttemp-- == *srctemp--;
+	}
+	else
+		ft_memcpy(dest, src, c);
+	return (dest);
+}
