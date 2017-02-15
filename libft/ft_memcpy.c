bd8e28bb46b2f42764c1530f6b7a84ac86b99a23
jmagnier <magnier.jeremy.pro@gmail.com>
2017-02-15 12:14:49 +0100

    15/02

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -1,5 +1,17 @@
 #include <libft.h>
 
-void *ft_memcpy(void *restric dst, const void *restrict src, size_t n)
+void *ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
+	char *p_dst;
+	const char *p_src;
+
+	*p_dst = dst;
+	*p_src = src;
+
+	while (n--)
+	{	
+		*p_dst++ = *p_src++;	
+	}
+
+	return (dst);
 }
